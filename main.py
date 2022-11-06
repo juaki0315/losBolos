8ab230592f7f0402df5bdec9177efd7ecfc413a4
juaki0315 <juaki0315@gmail.com>
2022-11-06 14:40:42 +0100

    Segundo test

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 
 class Partida():
-    partida = [(0,0)]*10
+
+    partida = ([0,0])*10
+
+    def sumar_puntuaciones(partida):
+        resultado = sum(list(map(sum, list(partida)))) 
+        return resultado
+        
+
--- test.py
+++ test.py
@@ -6,8 +6,14 @@ class TestBolos(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_existe_partida(self):
-        esperado = [(0,0)]*10
+        esperado = ([0,0])*10
         resultado = Partida.partida
+        self.assertEqual(esperado, resultado)
+    
+    def test_sumar_puntuacion(self):
+        partida = ([1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0])
+        esperado = 10
+        resultado = Partida.sumar_puntuaciones(partida)
         self.assertEqual(esperado, resultado)
 
 
