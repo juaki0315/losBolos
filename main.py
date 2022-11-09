dcf93e8e771ace720643f3a564aa5b1f1670d65a
juaki0315 <juaki0315@gmail.com>
2022-11-09 17:05:18 +0100

    Refactor usando objetos

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ class Partida:
 
     partida = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 
-    def sumar_puntuaciones(partida):
-        resultado = sum(list(map(sum, list(partida)))) 
+    def sumar_puntuaciones(self):
+        resultado = sum(list(map(sum, list(self.partida)))) 
         return resultado
 
-    def lanzar_bolas():
+    def lanzar_bolas(self):
         tirada = []
         tirada.append(random.randint(0,10))
         tirada.append(random.randint(0,(10 - tirada[0])))
         return tirada
 
-    def asignar_puntuacion(posicion, puntos):
-        Partida.partida[posicion] = puntos
-        return Partida.partida
+    def asignar_puntuacion(self, posicion, puntos):
+        self.partida[posicion] = puntos
+        return self.partida
 
 
--- test.py
+++ test.py
@@ -7,35 +7,42 @@ class TestBolos(unittest.TestCase):
 
     def test_existe_partida(self):
         esperado = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
-        resultado = Partida.partida
-        Partida.partida[0] = [0,0]
+        prueba = Partida()
+        resultado = prueba.partida
+        prueba.partida[0] = [0,0]
         self.assertEqual(esperado, resultado)
     
     def test_sumar_puntuacion(self):
-        partida = [[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0]]
+        prueba = Partida()
+        prueba.partida = [[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0]]
         esperado = 10
-        resultado = Partida.sumar_puntuaciones(partida)
+        resultado = prueba.sumar_puntuaciones()
         self.assertEqual(esperado, resultado)
 
     def test_sumar_puntuacion2(self):
-        partida = [[1,1],[1,0],[1,0],[1,0],[1,5],[1,0],[1,0],[1,0],[1,0],[1,0]]
-        esperado = 16
-        resultado = Partida.sumar_puntuaciones(partida)
+        prueba = Partida()
+        prueba.partida = [[1,1],[1,0],[1,0],[1,0],[1,5],[1,0],[1,0],[1,0],[1,0],[1,0]]
+        esperado = 16  
+        resultado = prueba.sumar_puntuaciones()
         self.assertEqual(esperado, resultado)
 
     def test_lanzar_bolas(self):
-        tirada = Partida.lanzar_bolas()
+        prueba = Partida()
+        tirada = prueba.lanzar_bolas()
         self.assertLessEqual(sum(tirada), 10)
 
     def test_lanzar_bolas2(self):
-        tirada = Partida.lanzar_bolas()
+        prueba = Partida()
+        tirada = prueba.lanzar_bolas()
         self.assertGreaterEqual(sum(tirada), 0)
 
     def test_asignar_puntuacion(self):
-        tirada = Partida.lanzar_bolas()
-        partida = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
-        partida[0] = tirada
-        self.assertEqual(partida, Partida.asignar_puntuacion(0, tirada))
+        prueba = Partida()
+        tirada = prueba.lanzar_bolas()
+        escenario = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+        escenario[0] = tirada
+        self.assertEqual(escenario, prueba.asignar_puntuacion(0, tirada))
+        print(prueba.partida)
 
 
 if __name__ == '__main__':
