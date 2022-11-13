58cfbd6d7be75f533e2393dc4fcddf77fc3d200c
juaki0315 <juaki0315@gmail.com>
2022-11-13 16:47:02 +0100

    refactor de metodo de sumar puntuaciones

--- main.py
+++ main.py
@@ -6,7 +6,25 @@ class Partida:
         self.tabla_puntos = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 
     def sumar_puntuaciones(self):
-        resultado = sum(list(map(sum, list(self.tabla_puntos)))) 
+        # resultado = sum(list(map(sum, list(self.tabla_puntos)))) 
+        resultado = 0
+        ha_habido_strike = False
+        for elemento in self.tabla_puntos:
+            posicion = 0
+            turno = self.tabla_puntos[posicion]
+            print(self.check_strike(turno))
+            if self.check_strike(turno):
+                resultado += 10
+                ha_habido_strike = True
+                posicion += 1 
+            else: 
+                if ha_habido_strike:
+                    resultado += 2*(sum(elemento))
+                    ha_habido_strike = False
+                else:
+                    resultado += sum(elemento)
+                posicion += 1
+
         return resultado
 
     def lanzar_bolas(self):
